f6f9ccc2654881c7ef261b763ca4e5032f26f41b
Jess Kreps <jrkreps@gmail.com>
2018-04-26 00:24:22 +0000

    building blocks

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,16 +25,23 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    @post.user_id = current_user.id
+    
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
     end
+
+    # respond_to do |format|
+    #   if @post.save
+    #     format.html { redirect_to @post, notice: 'Post was successfully created.' }
+    #     format.json { render :show, status: :created, location: @post }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @post.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /posts/1
