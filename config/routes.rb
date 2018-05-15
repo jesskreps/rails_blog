f87caaf2ac45736c07038ec1936e4dbce2d1ea9d
Jess Kreps <jrkreps@gmail.com>
2018-05-15 00:39:33 +0000

    api changes with ajax jquery

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root 'posts#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  get "api/v1/weather" => "api/v1/weather#get_weather"
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
